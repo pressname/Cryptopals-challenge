cef73cd53dc9982808e2d128c4f3c69fd7f44649
cleme <cleme@WOPR>
2021-05-29 16:08:26 +0200

    Add project files.

---
+++ Pages/CryptoChallengeSet1.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptopals.Pages
+{
+    public partial class CryptoChallengeSet1
+    {
+        private string HexString { get; set; } = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+
+        private static byte[] ConvertHexStringToByteArray(string value)
+        {
+            if (value.Length % 2 == 0)
+            {
+                throw new Exception("cannot have an odd number of digits");
+            }
+        }
+
+        private static string ConvertHexStringToBase64String(string value)
+        {
+
+        }
+    }
+}
