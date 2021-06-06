a961bb3b548e8fe90e526d50765d19e04011c274
pressname <pressname@protonmail.ch>
2021-06-06 13:12:15 +0200

    Update Xor function

--- Pages/CryptoChallengeSet1.razor.cs
+++ Pages/CryptoChallengeSet1.razor.cs
@@ -1,6 +1,7 @@
 ﻿using Cryptopals.Data;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace Cryptopals.Pages
 {
     public partial class CryptoChallengeSet1
     {
+        private double ElapsedMs { get; set; }
+        public List<string> Challenge4List { get; set; } = new();
         private string HexStringChallange1 { get; set; } = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         private readonly Dictionary<char, double> MostUsedCharFrequencyDict = new()
         {
@@ -25,6 +28,12 @@ namespace Cryptopals.Pages
             {'d',4.14 },
             {'u',2.85 }
         };
+
+        protected override async Task OnInitializedAsync()
+        {
+            await ImportTextFile();
+        }
+
         private string HexStringToBase64String(string value)
         {
             return Convert.ToBase64String(StringToByteArray(value));
@@ -62,23 +71,58 @@ namespace Cryptopals.Pages
 
         private List<XorResult> CrackSingleByteXOR(string value)
         {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
             var hexValue = StringToByteArray(value);
             var xordValues = new List<XorResult>();
-            for (char i = 'A'; i <= 'Z'; i++)
+            foreach (var key in Enumerable.Range(0, 127))
             {
+                var keyAsHex = Convert.ToChar(key);
                 var result = new byte[hexValue.Length];
                 for (int x = 0; x < hexValue.Length; x++)
                 {
-                    result[x] = (byte)(hexValue[x] ^ Convert.ToByte(i));
+                    result[x] = (byte)(hexValue[x] ^ Convert.ToByte(keyAsHex));
                 }
                 var plaintext = Encoding.UTF8.GetString(result);
-                //todo add scoring methode 
-                xordValues.Add(new XorResult(i, plaintext, MostUsedCharFrequencyDict));
-
+                xordValues.Add(new XorResult(keyAsHex, plaintext, MostUsedCharFrequencyDict));
             }
+            watch.Stop();
+            ElapsedMs = watch.Elapsed.TotalMilliseconds;
             return xordValues.OrderByDescending(x => x.CharFrequencyRating).ToList();
         }
 
+        private async Task ImportTextFile()
+        {
+            var data = await File.ReadAllLinesAsync("C:/Users/cleme/source/repos/Cryptopals/Data/challenge_4_data.txt");
+            Challenge4List = data.ToList();
+        }
+        private List<XorResult> CrackSingleByteXORFromFile(List<string> importedList)
+        {
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            var xordValues = new List<XorResult>();
+            for (int y = 0; y < importedList.Count; y++)
+            {
+                var hexValue = StringToByteArray(importedList[y].Trim().ToLower());
+                foreach(var key in Enumerable.Range(0, 127))
+                {
+                    var keyAsHex = Convert.ToChar(key);
+                    var result = new byte[hexValue.Length];
+                    for (int x = 0; x < hexValue.Length; x++)
+                    {
+                        result[x] = (byte)(hexValue[x] ^ Convert.ToByte(keyAsHex));
+                    }
+                    var plaintext = Encoding.UTF8.GetString(result);
+                    //todo add scoring methode 
+                    xordValues.Add(new XorResult(keyAsHex, plaintext, MostUsedCharFrequencyDict));
+                }
+            }
+
+            
+            watch.Stop();
+            ElapsedMs = watch.Elapsed.TotalMilliseconds;
+            return xordValues.OrderByDescending(x => x.CharFrequencyRating).ToList();
+        }
 
     }
 }
